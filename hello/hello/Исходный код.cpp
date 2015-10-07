27356ff138f22026701eb6cb1da7ca0be5c6e81e
Rostyslav Morozyuk <rmorozyukua@gmail.com>
2015-10-07 19:33:21 +0300

    con

--- hello/hello/Исходный код.cpp
+++ hello/hello/Исходный код.cpp
@@ -4,16 +4,9 @@ using namespace std;
 
 int main(void)
 {
-	Point point1, point2;
+	Point point1(3, 5, '#'), point2(5, 10, '*');
 
-	point1.x = 3;
-	point1.y = 2;
-	point1.symb = '*';
 	point1.Draw();
-
-	point2.x = 5;
-	point2.y = 0;
-	point2.symb = '#';
 	point2.Draw();
 
 	system("pause");
